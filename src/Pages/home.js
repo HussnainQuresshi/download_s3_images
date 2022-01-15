76e0df48ebccbcd8930b90bffd532026639073d2
Marwa <marwa.hassan@dyanamologic.com>
2022-01-15 19:41:00 +0500

    update

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -33,9 +33,15 @@ function Copyright(props) {
     >
       {"Copyright © "}
       <Link color="inherit" href="#">
-        Profile-Pic-Generator (kindly send the money through wiseTransfer if you
-        have an account its free of cost other wise its 1$max on fiver they will
-        charge you 2.5$ and they will charge me 20% )
+        Profile-Pic-Generator
+        <br />
+        kindly send the money through wiseTransfer if you have an account its
+        free of cost other wise its 1$max on fiver they will charge you 2.5$ and
+        they will charge me 20%.
+        <br />
+        Whatsapp:+923118423854
+        <br />
+        WiseAccountEmail: hussnainhashmi77133@gmail.com (Hussnain)
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
